1bf3e41bdd4939e553550b2a2280d5cbe70d8505
bnb-alexlucaci <alexandru.lucaci@bnbchain.org>
2023-10-31 22:17:17 +0200

    entrypoint: added working_directory

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,5 +8,9 @@ fi
 #then INPUT_OPTIONS="$INPUT_OPTIONS --network $INPUT_DOCKER_NETWORK"
 #fi
 
+if [ ! -z $INPUT_WORKING_DIRECTORY ];
+then cd $INPUT_WORKING_DIRECTORY
+fi
+
 exec $INPUT_RUN
 #exec docker run -v "/var/run/docker.sock":"/var/run/docker.sock" $INPUT_OPTIONS --entrypoint=$INPUT_SHELL $INPUT_IMAGE -c "${INPUT_RUN//$'\n'/;}"
